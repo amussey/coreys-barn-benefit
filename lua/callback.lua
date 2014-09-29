65f22049dbc805ccedc0ba88c233b1ad0ca11b4c
Andrew Mussey <musseyam@vt.edu>
2014-09-28 21:16:55 -0400

    Added the confirmation email into the lua callback.

--- lua/callback.lua
+++ lua/callback.lua
@@ -1,6 +1,26 @@
 local key = '<STRIPE PRIVATE API KEY>'
+local USERNAME = '<GMAIL USERNAME>'
+local PASSWORD = '<GMAIL PASSWORD>'
+local SERVER = 'smtp.gmail.com'
+local FROM_ADDRESS = USERNAME .. '@gmail.com'
+local TO_ADDRESS = '<TO EMAIL ADDRESS>'
+
+if request.method ~= "POST" then
+    return 302, '', {Location='https://amussey.github.com/coreys-barn-benefit/'}
+end
+
+if storage.callbacks ~= nil then
+    storage.callbacks = "{}"
+    callbacks = {}
+else
+    storage.callbacks = json.parse(storage.callbacks)
+end
 
 local callback = json.parse(request.form.token)
+
+table.insert(callbacks, callback)
+storage.callbacks = json.stringify(callbacks)
+
 
 local charge = http.request({
     method='post',
@@ -14,9 +34,25 @@ local charge = http.request({
     }})
 
 if charge.statuscode == 200 then
-    return "Thank you for ordering a " .. callback.order .. "!"
+    local lustache = require 'lustache'
+    local response = http.request {
+        url = 'https://amussey.github.io/coreys-barn-benefit/lua/email.html',
+    }
+
+
+    emailbody = lustache:render(response.content, {["name"]="Andrew", ["mailto"]=FROM_ADDRESS})
+
+    email.send {
+        server=SERVER, username=USERNAME, password=PASSWORD,
+        from=FROM_ADDRESS,
+        to=TO_ADDRESS,
+        subject='Corey\'s Barn Benefit: Donation received',
+        html=emailbody
+    }
+
+    return 302, '', {Location='https://amussey.github.com/coreys-barn-benefit/thankyou'}
 else
-    return "Charge rejected for a " .. callback.order .. "!"
+    return "Your charge for $" .. (callback.charge/100) .. " failed.  Please contact " .. FROM_ADDRESS .. "."
 end
 
 return callback
